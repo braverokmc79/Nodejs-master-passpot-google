dce3845aa52f3db9fd52f431a7a6b080f3ae855e
braverokmc79 <braverokmc79@gmail.com>
2022-10-06 16:31:36 +0900

    first commit

---
+++ sechema.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) unique NOT NULL,
+  `email` varchar(30) unique NOT NULL,
+  `hashed_password` varchar(100) DEFAULT NULL,
+  `salt` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
